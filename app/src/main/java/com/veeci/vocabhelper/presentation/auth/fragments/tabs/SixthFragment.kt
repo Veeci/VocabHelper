481fbc682eca0f5addfd1cfd2b106eb0c04e3223
Veeci <doanvietquang15102003@gmail.com>
2024-09-22 00:14:28 +0700

    modified onboarding tutorial

---
+++ app/src/main/java/com/veeci/vocabhelper/presentation/auth/fragments/tabs/SixthFragment.kt
@@ -0,0 +1,42 @@
+package com.veeci.vocabhelper.presentation.auth.fragments.tabs
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import com.veeci.vocabhelper.R
+import com.veeci.vocabhelper.databinding.FragmentSixthBinding
+
+class SixthFragment : Fragment() {
+
+    private var _binding: FragmentSixthBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSixthBinding.inflate(layoutInflater, container, false)
+
+        setupFunction()
+
+        return binding.root
+    }
+
+    private fun setupFunction() {
+        binding.buttonBack.setOnClickListener {
+            findNavController().navigate(R.id.action_fourthFragment_to_onboardingViewpagerFragment)
+        }
+
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_fourthFragment_to_loginFragment)
+        }
+
+        binding.signupButton.setOnClickListener {
+            findNavController().navigate(R.id.action_fourthFragment_to_signupFragment)
+        }
+    }
+
+}
